9ff9e6edc5e07c5f96d77c7f2bd914ef2ba44e83
bolamundi <104913925+bolamundi@users.noreply.github.com>
2022-07-07 22:09:37 -0300

    Banco de Dados 2.0
    Adicionadas as tabelas para avaliações, jogadores e seleções.

--- id18524654_loginbolamundi.sql
+++ id18524654_loginbolamundi.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.5
+-- version 4.7.0
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Tempo de geração: 26-Abr-2022 às 10:41
--- Versão do servidor: 10.5.12-MariaDB
--- versão do PHP: 7.3.32
+-- Host: 127.0.0.1
+-- Generation Time: 07-Jul-2022 às 04:23
+-- Versão do servidor: 5.7.17
+-- PHP Version: 5.6.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,8 +19,20 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Banco de dados: `id18524654_loginbolamundi`
+-- Database: `id18524654_loginbolamundi`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `avaliacoes`
+--
+
+CREATE TABLE `avaliacoes` (
+  `Id` int(11) NOT NULL,
+  `Estrelas` int(5) NOT NULL,
+  `Id_selecao` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -33,19 +45,56 @@ CREATE TABLE `comentarios` (
   `Comentario` varchar(800) COLLATE utf8_unicode_ci NOT NULL,
   `Nome` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
   `Data` date NOT NULL,
-  `Id_usuario` int(11) NOT NULL
+  `Id_usuario` int(11) NOT NULL,
+  `Id_selecao` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Extraindo dados da tabela `comentarios`
 --
 
-INSERT INTO `comentarios` (`Id`, `Comentario`, `Nome`, `Data`, `Id_usuario`) VALUES
-(64, 'Neymar craque de bola, gênio ', 'Lucas', '2022-04-17', 3),
-(65, 'Pega eles, pit', 'Lucas', '2022-04-17', 3),
-(66, 'Thales, onde quem é vc??????', 'Didico', '2022-04-17', 5),
-(67, 'quem é**', 'Didico', '2022-04-17', 5),
-(68, 'teste', 'Lucas', '2022-04-19', 3);
+INSERT INTO `comentarios` (`Id`, `Comentario`, `Nome`, `Data`, `Id_usuario`, `Id_selecao`) VALUES
+(64, 'Neymar craque de bola, gênio ', 'Lucas', '2022-04-17', 3, 0),
+(65, 'Pega eles, pit', 'Lucas', '2022-04-17', 3, 0),
+(66, 'Thales, onde quem é vc??????', 'Didico', '2022-04-17', 5, 0),
+(67, 'quem é**', 'Didico', '2022-04-17', 5, 0),
+(68, 'teste', 'Lucas', '2022-04-19', 3, 0),
+(69, 'Comentario', 'Lucas', '2022-07-07', 57, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `jogadores`
+--
+
+CREATE TABLE `jogadores` (
+  `Id` int(11) NOT NULL,
+  `Nome` varchar(40) NOT NULL,
+  `Numero` int(11) NOT NULL,
+  `Posicao` varchar(40) NOT NULL,
+  `Id_selecao` int(11) NOT NULL,
+  `Pais` varchar(54) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `selecoes`
+--
+
+CREATE TABLE `selecoes` (
+  `Id` int(11) NOT NULL,
+  `Pais` varchar(88) NOT NULL,
+  `Bandeira` varchar(88) NOT NULL,
+  `Ajetivo` varchar(88) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `selecoes`
+--
+
+INSERT INTO `selecoes` (`Id`, `Pais`, `Bandeira`, `Ajetivo`) VALUES
+(1, 'Brasil', '', 'Brasileira');
 
 -- --------------------------------------------------------
 
@@ -57,63 +106,85 @@ CREATE TABLE `usuarios` (
   `Id` int(11) NOT NULL,
   `Nome` varchar(50) NOT NULL,
   `Email` varchar(50) NOT NULL,
-  `Senha` varchar(50) NOT NULL,
-  `Acesso` varchar(10) NOT NULL
+  `Senha` varchar(400) NOT NULL,
+  `Acesso` varchar(10) NOT NULL,
+  `Data` date NOT NULL,
+  `Status` varchar(100) NOT NULL,
+  `Rank` varchar(100) NOT NULL DEFAULT '0',
+  `Saldo` int(100) NOT NULL DEFAULT '0',
+  `Xp` int(100) NOT NULL DEFAULT '0'
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `usuarios`
 --
 
-INSERT INTO `usuarios` (`Id`, `Nome`, `Email`, `Senha`, `Acesso`) VALUES
-(1, 'Marinho', 'Marinho@marinho.com', '123', 'Admin'),
-(2, 'Pedro queixada', 'pedro@gmail.com', 'abc', 'Admin'),
-(3, 'Lucas', 'lucas@bolamundi.com', '1234', 'Admin'),
-(5, 'Didico', 'vapo@vapo.com', '123', 'Admin'),
-(7, 'Caroline', 'caroline@bolamundi.com', 'gabigol9', 'Camisa 9'),
-(12, 'gabriel', 'gabriel@flamengo.com', '2332', ''),
-(22, 'Ana Clara', 'anaclara@bolamundi.com', 'aninha321', 'Camisa 2'),
-(24, 'Sofia', 'sofia@bolamundi.com', 'sofia123', 'Camisa 77'),
-(35, 'Coe tales, quem eh tu, meu patrão?', 'criaumperfil@parameresponder.com', '12345678Ab', 'Usuario'),
-(31, 'teste', 'teste@gmail.com', 'Teste123', 'Usuario'),
-(32, 'Suy', 'suy@teste.com', 'Susu12345', 'Usuario'),
-(33, 'Flamengo', 'rubroNegro@gmail.com', 'Paralelepipedo8', 'Usuario'),
-(34, 'Thales', 'pretoescuro@gmail.com', 'pRETOESCURO123', 'Usuario'),
-(55, 'Ana Eduarda', 'anaeduardamartins04@gmail.com', '123Anime', 'Usuario'),
-(56, 'Joao pé de feijçao', 'feijao@joao.com', '123', 'Admin');
+INSERT INTO `usuarios` (`Id`, `Nome`, `Email`, `Senha`, `Acesso`, `Data`, `Status`, `Rank`, `Saldo`, `Xp`) VALUES
+(57, 'Lucas', 'lucas@bolamundi.com', '$2y$10$Xy28YXMQKSMfokA2wJuituQMH70kU9urrbTeSyVBgppEc5B/zoES6', 'Admin', '2022-07-07', 'Ativo', '0', 0, 0);
 
 --
--- Índices para tabelas despejadas
+-- Indexes for dumped tables
 --
 
 --
--- Índices para tabela `comentarios`
+-- Indexes for table `avaliacoes`
+--
+ALTER TABLE `avaliacoes`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Indexes for table `comentarios`
 --
 ALTER TABLE `comentarios`
   ADD PRIMARY KEY (`Id`);
 
 --
--- Índices para tabela `usuarios`
+-- Indexes for table `jogadores`
+--
+ALTER TABLE `jogadores`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Indexes for table `selecoes`
+--
+ALTER TABLE `selecoes`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Indexes for table `usuarios`
 --
 ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`Id`);
 
 --
--- AUTO_INCREMENT de tabelas despejadas
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT de tabela `comentarios`
+-- AUTO_INCREMENT for table `avaliacoes`
+--
+ALTER TABLE `avaliacoes`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `comentarios`
 --
 ALTER TABLE `comentarios`
-  MODIFY `Id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=69;
-
+  MODIFY `Id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=70;
 --
--- AUTO_INCREMENT de tabela `usuarios`
+-- AUTO_INCREMENT for table `jogadores`
+--
+ALTER TABLE `jogadores`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `selecoes`
+--
+ALTER TABLE `selecoes`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
-COMMIT;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=58;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
